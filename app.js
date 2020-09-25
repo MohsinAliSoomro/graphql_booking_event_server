04f6898e76059f85388fa4ed4474362a96c40e7b
MohsinAliSoomro <mohsinalisoomro23@gmail.com>
2020-09-25 10:40:14 +0500

    configure initial graphql express app

--- app.js
+++ app.js
@@ -1,13 +1,41 @@
-const express = require('express');
-const bodyParser= require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 //endpoint
-app.get('/',(req,res,next)=>{
-    res.send('Hello world');
-})
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: buildSchema(`
+    type RootQuery {
+        events: [String!]!
+    }
 
-app.listen(4000,()=>console.log("server is runnin on port of 4000"))
+    type RootMutation {
+        createEvent(name:String): String
+    }
+
+    schema {
+        query: RootQuery
+        mutation: RootMutation
+    }
+    `),
+    rootValue: {
+      events: () => {
+        return ["Mohsin", "Ali", "Soomro"];
+      },
+      createEvent: (args) => {
+        const eventName = args.name;
+        return eventName;
+      },
+    },
+    graphiql: true,
+  })
+);
+
+app.listen(4000, () => console.log("server is running on port of 4000"));
