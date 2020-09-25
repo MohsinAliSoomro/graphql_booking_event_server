52b4918ffa41ca326eaeeac8e152c530e7396ed6
MohsinAliSoomro <mohsinalisoomro23@gmail.com>
2020-09-25 11:16:18 +0500

    the create  event type

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const { buildSchema } = require("graphql");
 
 const app = express();
 
+const events=[];
+
 app.use(bodyParser.json());
 
 //endpoint
@@ -12,12 +14,28 @@ app.use(
   "/graphql",
   graphqlHTTP({
     schema: buildSchema(`
+    type Event {
+      _id:ID!
+      title:String!
+      description:String!
+      price:Float!
+      date:String!
+    }
+
+    input EventInput{
+      title:String!
+      description:String!
+      price:Float!
+      date:String!
+    }
+
+
     type RootQuery {
-        events: [String!]!
+        events: [Event!]!
     }
 
     type RootMutation {
-        createEvent(name:String): String
+        createEvent(eventInput:EventInput): Event
     }
 
     schema {
@@ -27,11 +45,18 @@ app.use(
     `),
     rootValue: {
       events: () => {
-        return ["Mohsin", "Ali", "Soomro"];
+        return events;
       },
       createEvent: (args) => {
-        const eventName = args.name;
-        return eventName;
+        const event = {
+          _id: Math.random().toString(),
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: args.eventInput.date,
+        };
+        events.push(event)
+        return event;
       },
     },
     graphiql: true,
